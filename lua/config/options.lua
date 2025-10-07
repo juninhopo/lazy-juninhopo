a28ab36cb56847146c16ffa13f6aed4e94754b7f
juninhopo <kelgaxd@gmail.com>
2025-10-07 19:30:40 -0300

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,12 @@
 -- Add any additional options here
 vim.g.snacks_animate = false
 
+-- Configuração da fonte Victor Mono
+-- Para GUI (Neovide, Neovim-Qt, etc.)
+vim.opt.guifont = "VictorMono Nerd Font:h12"
+
+-- Fallback para ícones (caso necessário)
+-- vim.opt.guifontwide = "Symbols Nerd Font Mono:h12"
+
 -- Configuração para ativar o suporte ao clipboard
 vim.opt.clipboard = "unnamedplus"
