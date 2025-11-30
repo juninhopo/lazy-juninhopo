33d8fe4de1b5581b77b68eec4db4921e2d2951e7
juninhopo <kelgaxd@gmail.com>
2025-11-30 19:41:17 -0300

    update

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -25,5 +25,35 @@ return {
   {
     "mg979/vim-visual-multi",
     event = "VeryLazy"
+  },
+
+  -- GitHub Issues integration
+  {
+    "juninhopo/issues-neovim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = false,
+    branch = "main",
+    config = function()
+      require("issues_neovim").setup({
+        enabled = true,
+        keys = {
+          open = "<leader>gi",
+          close = "q",
+          refresh = "r",
+          navigate = { prev = "k", next = "j" },
+          view_details = "<CR>",
+        },
+        ui = {
+          width = 0.8,
+          height = 0.8,
+          border = "rounded",
+        },
+        github = {
+          api_url = "https://api.github.com",
+        }
+      })
+    end,
   }
 }
