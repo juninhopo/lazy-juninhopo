e724a8204ca052950571518bdd06d0aef43b64f1
Darlan Jr. <kelgaxd@gmail.com>
2026-05-12 02:05:02 -0400

    remove wakatime plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,11 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
   },
 }
